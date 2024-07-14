92a867604c337e14513a78979cd6c92305561522
BIYANOOR <160410736+BIYANOOR@users.noreply.github.com>
2024-07-14 19:12:24 +0500

    Add files via upload

---
+++ main.ts
@@ -0,0 +1,30 @@
+#!/bin/env node;
+            import inquirer from "inquirer";
+console.log("🏵️🏵️  Wellcome to Sobia Furqan's TODO LIST APP  🏵️🏵️")
+let condition = true;
+let todos: string[] = [];
+
+async function runTodoApp() {
+    while (condition) {
+        const todoQuestions = await inquirer.prompt([
+            {
+                name: "firstQuestion",
+                type: "input",
+                message: "🏵️   What would you like to add  in your todo list  🏵️?",
+            },
+            {
+                name: "secondQuestion",
+                type: "confirm",
+                message: "🏵️   Would you like to add more in your todos or want to exit from todos?  🏵️",
+                default: false,
+            },
+        ]);
+
+        todos.push(todoQuestions.firstQuestion);
+        console.log(todos);
+        condition = todoQuestions.secondQuestion;
+        console.log("🏵️   your todos add successfully ☝️   see your updated todos list🏵️  ")
+    }
+}
+
+runTodoApp();
